777101fc8039a87566c6936219a41e6a111a3aa4
Kal Marsh <kal.marsh@gov.bc.ca>
2024-07-08 18:43:36 -0700

    adds more validation to program years

--- app/Listeners/ProcessUpdatedProgramYear.php
+++ app/Listeners/ProcessUpdatedProgramYear.php
@@ -4,34 +4,42 @@ namespace App\Listeners;
 
 use App\Events\ProgramYearUpdated;
 use App\Models\Allocation;
-use App\Models\Claim;
-use App\Models\Program;
 use App\Models\ProgramYear;
-use App\Models\Student;
-use Illuminate\Support\Facades\Log;
 
 class ProcessUpdatedProgramYear
 {
 
     /**
      * Handle the event.
+     * This will update the status of all allocations under the modified program year to match the program year's status.
+     * If the modified record status is set to active then all other program years and associated allocations would be switched to inactive.
      */
     public function handle(ProgramYearUpdated $event): void
     {
         // Update the status of all allocations associated with this program year to match the PY status
-        $programYearId = $event->programYear;
+        $programYearId = $event->programYear->id;
         $status = $event->status;
-        $programYear = ProgramYear::where('id', $programYearId->id)->with('allocations')->first();
+        $programYear = ProgramYear::with('allocations')->find($programYearId);
 
-
-        \Log::info('Start handling program year update to status: ' . $status);
-
-        if (!is_null($programYear)) {
-            \Log::info('start_date: '.$programYear->start_date);
+        if ($programYear) {
+            // Update the status of the program year's allocations
             foreach ($programYear->allocations as $allocation) {
-                \Log::info('allocation: '.$allocation->id);
                 $allocation->status = $status;
                 $allocation->save();
+            }
+
+            // Deactivate other program years if the status is active
+            if ($status === 'active') {
+                // Deactivate all other program years
+                ProgramYear::where('id', '!=', $programYearId)->update(['status' => 'inactive']);
+
+                // Get IDs of allocations associated with the old program years
+                $allocationIds = Allocation::whereHas('py', function ($query) use ($programYearId) {
+                    $query->where('id', '!=', $programYearId);
+                })->pluck('id');
+
+                // Batch update the status of the allocations
+                Allocation::whereIn('id', $allocationIds)->update(['status' => 'inactive']);
             }
         }
     }
