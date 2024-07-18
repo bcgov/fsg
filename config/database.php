06b5da055f6056e8487954263e72e04585dca801
Kal Marsh <kal.marsh@gov.bc.ca>
2024-07-18 09:15:02 -0700

    updates dockerfile

--- config/database.php
+++ config/database.php
@@ -152,8 +152,8 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
         ],
 
         'cache' => [
@@ -161,8 +161,8 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
         ],
 
     ],
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
 Route::get('/logout', [UserController::class, 'logout'])->name('get-logout');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
@@ -13,4 +14,14 @@ Route::get('/applogin', [UserController::class, 'appLogin'])->name('app-login');
 Route::get('/bceid-login', [UserController::class, 'bceidLogin'])->name('bceid-login');
 Route::get('/bcsc-login', [UserController::class, 'bcscLogin'])->name('bcsc-login');
 Route::middleware(['auth'])->get('/home', [UserController::class, 'home'])->name('home');
+
+
+Route::get('/test-redis', function () {
+    try {
+        $name = Redis::get('name');
+        return "Redis connection successful. Name: " . $name;
+    } catch (Exception $e) {
+        return "Redis connection failed: " . $e->getMessage();
+    }
+});
 
