e4d61de06ac025f6b39c927cb83a18a0166896de
Kal Marsh <kal.marsh@gov.bc.ca>
2024-07-21 00:56:29 -0700

    adds midnight job

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ fi
 echo "ENV_ARG: ${ENV_ARG}"
 
 # Add cron job for Laravel schedule:run
-echo "* * * * * cd /var/www/html && php artisan schedule:run >> /dev/null 2>&1" | crontab -
+#echo "* * * * * cd /var/www/html && php artisan schedule:run >> /dev/null 2>&1" | crontab -
 
 echo "Install composer"
 composer dump-autoload
