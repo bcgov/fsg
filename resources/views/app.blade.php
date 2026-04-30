a3b4b81022a03325bf18e01d4c2cda0b7cc9c21b
AimeeGao <Aimee.Gao@gov.bc.ca>
2026-04-30 11:31:40 -0700

    Replace mix helper

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
+    @vite(['resources/js/app.js'])
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" integrity="sha384-XGjxtQfXaH2tnPFa9x+ruJTuLE3Aa6LhHSWRr1XeTyhezb4abCG4ccI5AkVDxqC+" crossorigin="anonymous">
@@ -18,8 +18,6 @@
     <link rel="icon" href="{{ asset('/images/favicon.ico') }}" type="image/vnd.microsoft.icon">
     <link rel="icon" type="image/x-icon" href="{{ asset('/images/favicon.ico') }}">
 
-    <!-- Scripts -->
-    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
 
     @inertiaHead
 </head>
