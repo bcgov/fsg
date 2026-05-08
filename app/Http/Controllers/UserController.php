a2f3783ab0aa6f2eef60a7d03a4255d1a8f9b0d0
Kal Marsh <kal.marsh@gov.bc.ca>
2026-05-08 10:59:14 -0700

    adds pdex ind token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,6 @@ use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
@@ -166,8 +165,8 @@ class UserController extends Controller
             if (is_null($user)) {
                 [$valid, $user] = $this->newUser($provider_user, $type);
                 if ($valid == '200' && $type === Role::Student) {
-
-                    Cache::put('bcsc_provider_user_' . $user->id, json_encode($provider_user));
+                    $request->session()->put('bcsc_provider_user_' . $user->id, json_encode($provider_user));
+                    // Cache::put('bcsc_provider_user_' . $user->id, json_encode($provider_user));
                     Auth::login($user);
 
                     \Log::info(' ');
@@ -223,7 +222,7 @@ class UserController extends Controller
             }
 
             if ($type === Role::Student) {
-                Cache::put('bcsc_provider_user_' . $user->id, json_encode($provider_user));
+                $request->session()->put('bcsc_provider_user_' . $user->id, json_encode($provider_user));
                 Auth::login($user);
 
                 \Log::info(' ');
@@ -385,13 +384,12 @@ class UserController extends Controller
             \Log::info('New user. Attempting to register.');
             [$valid, $user] = $this->newUser($decodedToken['payload'], $type);
             if ($valid == '200' && $type === Role::Student) {
-
                 // Cache the provider user data for later use in the application, such as displaying user info on the frontend
                 if (!is_null($decodedIndividualToken)) {
-                    Cache::put('bcsc_pdex_individual_' . $user->id, json_encode($decodedIndividualToken));
+                    $request->session()->put('bcsc_pdex_individual_' . $user->id, json_encode($decodedIndividualToken));
                 }
 
-                Cache::put('bcsc_provider_user_' . $user->id, json_encode($decodedToken['payload']));
+                $request->session()->put('bcsc_provider_user_' . $user->id, json_encode($decodedToken['payload']));
                 Auth::login($user);
 
                 \Log::info(' ');
@@ -475,9 +473,14 @@ class UserController extends Controller
 
         if ($type === Role::Student) {
             \Log::info('User is Student. Logging in.');
-            Cache::put('bcsc_provider_user_' . $user->id, json_encode($decodedToken['payload']));
+            $request->session()->put('bcsc_provider_user_' . $user->id, json_encode($decodedToken['payload']));
             Auth::login($user);
             $request->session()->put('bcsc_logout_uri', $logoutUrl);
+
+                // Cache the provider user data for later use in the application, such as displaying user info on the frontend
+                if (!is_null($decodedIndividualToken)) {
+                    $request->session()->put('bcsc_pdex_individual_' . $user->id, json_encode($decodedIndividualToken));
+                }
 
             return Redirect::route('student.home');
         }
